2b0cb53a6b01471b2a2c6dfb590d5b37d2c9174f
Bobby Luly <bobbyluly@gmail.com>
2023-08-25 11:53:44 -0600

    Create functions to pass tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,18 @@
 
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
+  createDragon : function(name, rider, temperment) { 
+    return {
+      name: name,
+      rider: rider,
+      temperment: temperment
+    }
+  },
+  greetRider : function(dragon) {
+    return `Hi, ${dragon.rider}!`
+  },
+  eat : function() {
+    return this.hungry = false
+    },
   // findFireBreathers
 }
